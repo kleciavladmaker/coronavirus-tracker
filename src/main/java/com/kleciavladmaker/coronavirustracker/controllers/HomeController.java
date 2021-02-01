9c574f2d52ead26a94b9adcc8f8ba829020ad336
Klecia Vladmaker <klecia.vladmaker@hotmail.com>
2021-02-01 15:52:17 -0300

    Alterações no HTML

--- src/main/java/com/kleciavladmaker/coronavirustracker/controllers/HomeController.java
+++ src/main/java/com/kleciavladmaker/coronavirustracker/controllers/HomeController.java
@@ -19,8 +19,10 @@ public class HomeController {
     public String home(Model model) {
         List<LocationStats> allStats = coronaVirusDataService.getAllStats();
         int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
         model.addAttribute("locationStats", allStats);
         model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
 
 
         return "home";
