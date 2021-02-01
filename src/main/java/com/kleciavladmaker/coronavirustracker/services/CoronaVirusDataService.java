b9dba28d47cdeb0cf5a0d6354651dad17560baec
Klecia Vladmaker <klecia.vladmaker@hotmail.com>
2021-02-01 12:39:41 -0300

    Data Service

---
+++ src/main/java/com/kleciavladmaker/coronavirustracker/services/CoronaVirusDataService.java
@@ -0,0 +1,35 @@
+package com.kleciavladmaker.coronavirustracker.services;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Service
+public class CoronaVirusDataService {
+
+    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+    @PostConstruct
+    @Scheduled(cron = "* * 1 * * *")
+    public void fetchVirusData() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(VIRUS_DATA_URL)).build();
+        HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+        for (CSVRecord record : records) {
+            String state = record.get("Province/State");
+            System.out.println(state);
+        }
+    }
+}
